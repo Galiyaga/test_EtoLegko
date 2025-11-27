20d1d49a6ff760a2bd5baef02ca4595b2bea01df
Galiya <galiyaga@yandex.ru>
2025-11-27 17:13:25 +0300

    Проверено на локальном сервере, что код статуса запроса на api.php 200

--- api.php
+++ api.php
@@ -1,8 +1,11 @@
 <?php
+
+/* Проверено на локальном сервере, что код статуса запроса на api.php 200 */
+
 define('PATH_KEITARO_ORDERS_API_SRC','../../../orders_api/src/');
 require_once(PATH_KEITARO_ORDERS_API_SRC.'send_order.php');
 
-$thxPage = 'success.html';
+$thxPage = './success/success.html';
 $dimensionName = 'default';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
